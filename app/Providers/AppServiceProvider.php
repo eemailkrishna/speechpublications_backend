e545832217eb0ee8bfacc57f865a1ea72e671351
krishna <eemail.krishna@gmail.com>
2026-09-18 21:46:13 +0530

    fix error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,22 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Ensure Reverb config has all required keys
+        if (!config('reverb.servers.reverb.scaling')) {
+            config(['reverb.servers.reverb.scaling' => [
+                'enabled' => false,
+                'channel' => 'reverb',
+                'server' => [
+                    'url' => null,
+                    'host' => '127.0.0.1',
+                    'port' => '6379',
+                    'username' => null,
+                    'password' => null,
+                    'database' => '0',
+                    'timeout' => 60,
+                ],
+            ]]);
+        }
     }
 }
+
